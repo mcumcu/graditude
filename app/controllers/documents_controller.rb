01fdbbd014336e855a320cb96664bad16e1beae8
Christopher Unger <mcu@mac.com>
2025-09-05 15:53:32 -0400

    refactor-printable

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,7 +2,22 @@ class DocumentsController < ApplicationController
   include Printable
 
   def index
-    @params = default_params # .merge!(honoree_name: "foo")
+    @params = default_params.merge!(
+      graduate_name: "Meili Blaine Elizabeth Unger",
+      degree: "Bachelor of Science",
+      honoree_name: "Michael Christopher Unger",
+      major: "Organic Chemistry",
+      nouns: [
+        "love",
+        "support"
+      ],
+      message: [
+        "Gratias tibi ago pro amore et auxilio tuo"
+        # "Thank you for your love and support"
+      ],
+      presented_on: "May 2026",
+      signature_path: "givers_signature.png"
+    )
 
     respond_to do |format|
       format.html
