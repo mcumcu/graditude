6b88f29a3981d46981950705ca5e269758490905
Christopher Unger <mcu@mac.com>
2025-09-21 15:52:00 -0400

    add westtown

--- app/controllers/concerns/printable.rb
+++ app/controllers/concerns/printable.rb
@@ -13,8 +13,7 @@ module Printable
     IMG_DIR = "#{ASSET_DIR}/images"
   end
 
-  # Methods defined directly in the module become instance methods
-  # of the including controller
+  # Methods defined directly in the module become instance methods of the including controller
   def default_params
     {
       graduate_name: nil,
@@ -34,7 +33,17 @@ module Printable
     banner_path = "#{IMG_DIR}/penn.svg"
     seal_path = "#{IMG_DIR}/penn_seal.png"
 
-    @new_prawn_doc ||= PrawnRails::Document.new(page_size: "LETTER", page_layout: :landscape, margin: [ 0, 0, 0, 0 ]) do |pdf|
+    page_size = "LETTER"
+    page_layout = :landscape
+    page_margin = [ 0, 0, 0, 0 ]
+
+    prawn_document_params = {
+      page_size: page_size,
+      page_layout: page_layout,
+      margin: page_margin
+    }
+
+    @new_prawn_doc ||= PrawnRails::Document.new(prawn_document_params) do |pdf|
       pdf.font(document_font)
 
       pdf.float do
@@ -101,13 +110,13 @@ module Printable
       pdf.text_box graduate_name.to_s, align: :center, size: 6.mm, width: container_width, at: [ 0, pdf.cursor - 1.5.mm ]
     end
 
-    pdf.text_box [ degree, major ].compact.join(", "), align: :center, size: 4.mm, width: container_width, at: [ 0, pdf.cursor - 12.mm ]
+    pdf.text_box [ degree, major ].compact.join(", "), align: :center, size: 3.5.mm, width: container_width, at: [ 0, pdf.cursor - 12.mm ]
 
     # right column
     # if message.size == 2
     #   pdf.text_box message.first, align: :center, size: 6.mm, width: (half_width) - margin_horizontal, height: 30.mm, at: [ (half_width) + 10.mm, pdf.cursor - 10.mm ]
 
-    #   pdf.text_box message.last, align: :center, size: 4.mm, width: (half_width) - margin_horizontal, height: 30.mm, at: [ (half_width) + 10.mm, pdf.cursor - 19.mm ]
+    #   pdf.text_box message.last, align: :center, size: 3.5.mm, width: (half_width) - margin_horizontal, height: 30.mm, at: [ (half_width) + 10.mm, pdf.cursor - 19.mm ]
     # elsif message.size == 1
     #   pdf.text_box message.first, align: :center, size: 6.mm, width: (half_width) - margin_horizontal, height: 30.mm, at: [ (half_width) + 10.mm, pdf.cursor - 13.mm ]
     # end
@@ -117,8 +126,97 @@ module Printable
     pdf
   end
 
+  def new_westtown_doc
+    background = "#{IMG_DIR}/westtown_paper.jpg"
+    document_font = "#{FONT_DIR}/Jost-VariableFont_wght.ttf"
+    banner_path = "#{IMG_DIR}/westtown.svg"
+    seal_path = "#{IMG_DIR}/westtown_seal.svg"
+
+    page_size = [ 175.mm, 227.mm ]
+    page_layout = :landscape
+    page_margin = [ 0, 0, 0, 0 ]
+
+    prawn_document_params = {
+      page_size: page_size,
+      page_layout: page_layout,
+      margin: page_margin
+    }
+
+    @new_prawn_doc ||= PrawnRails::Document.new(prawn_document_params) do |pdf|
+      pdf.font(document_font)
+
+      pdf.float do
+        pdf.image background, width: pdf.margin_box.width, height: pdf.margin_box.height, position: :center
+      end
+
+      pdf.move_down(20.mm)
+
+      pdf.svg File.read(banner_path), width: 170.mm, position: :center
+
+      pdf.move_down(9.mm)
+
+      pdf.svg File.read(seal_path), width: 25.4.mm, position: :center
+
+      pdf.move_down(6.mm)
+
+      pdf.text "This is to certify that", align: :center, size: 3.5.mm
+
+      pdf.move_down(2.mm)
+    end
+  end
+
+  def make_westtown_document(params = {})
+    graduate_name = params["graduate_name"] || " "
+    degree = params["degree"]
+    honoree_name = params["honoree_name"] || " "
+    major = params["major"]
+    nouns = params["nouns"]&.reject { |n| n.empty? } || []
+    message = params["message"] || ""
+    presented_on = params["presented_on"]
+    name_font = "#{FONT_DIR}/Literata-SemiBold.ttf"
+    # signature_font = "#{FONT_DIR}/MySoul-Regular.ttf"
+    signature_font = "#{FONT_DIR}/HomemadeApple-Regular.ttf"
+
+    pdf = new_westtown_doc
+
+    pdf.font(name_font) do
+      pdf.text graduate_name.to_s, align: :center, size: 9.mm
+    end
+
+    pdf.move_down(3.mm)
+
+    pdf.text "has successfully completed the requirements for graduation", align: :center, size: 3.5.mm
+    # pdf.text "and in recognition thereof is awarded this diploma", align: :center, size: 3.5.mm
+    pdf.text nouns.any? ? "and hereby recognizes the #{nouns.join(' and ')} of" : nil, align: :center, size: 3.5.mm
+
+    pdf.move_down(3.mm)
+
+    pdf.font(name_font) do
+      pdf.text honoree_name.to_s, align: :center, size: 9.mm
+    end
+
+    pdf.move_down(3.mm)
+
+    pdf.text "given at Westtown School, this #{presented_on.split(' ').slice(0...-1).join(' ')}, #{presented_on.split(' ').last.to_i.humanize}.", align: :center, size: 3.5.mm
+
+    pdf.move_down(3.mm)
+
+    # left column
+    container_width = message.size == 0 ? width : half_width
+
+    pdf.font(signature_font) do
+      pdf.text_box graduate_name.to_s, align: :center, size: 4.mm, width: container_width, at: [ 0, pdf.cursor - 8.mm ]
+    end
+
+    pdf.text_box "Diplomate, #{presented_on.split(' ').last.to_i.humanize}", align: :center, size: 3.5.mm, width: container_width, at: [ 0, pdf.cursor - 14.mm ]
+
+    pdf.text_box message, align: :center, size: 7.mm, width: (half_width) - margin_horizontal, at: [ (half_width) + 10.mm, pdf.cursor - 5.mm ]
+
+    pdf
+  end
+
   def rerender_png_path
-    doc = make_document(@certificate&.data || default_params)
+    doc = make_westtown_document(@certificate&.data || default_params)
     doc.render_file(temp_pdf_path(@certificate&.id || "_blank"))
 
     page = PDFToImage.open(temp_pdf_path(@certificate&.id || "_blank")).first
