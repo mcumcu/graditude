63f4bc2f8066b3a0b48a4609a9b48d56554f48a4
Christopher Unger <mcu@mac.com>
2026-04-19 09:50:09 -0400

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def form_label_classes
-    "text-content-muted dark:text-content-muted-dark text-sm uppercase mb-2"
+    "text-content-muted text-sm uppercase mb-2"
   end
 
   def form_input_classes(certificate)
-    "block w-full px-3 py-2 shadow-sm rounded-md border border-black/30 dark:border-white/30 focus:outline-primary dark:focus:outline-primary-dark text-xl tracking-wide bg-background dark:bg-background-dark"
+    "block w-full px-3 py-2 shadow-sm rounded-md border border-black/30 dark:border-white/30 focus:outline-primary dark:focus:outline-primary-dark text-xl tracking-wide bg-background"
   end
 end
