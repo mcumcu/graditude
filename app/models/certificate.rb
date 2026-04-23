3c85254423184479fdf6fcccb308445c4147dd68
Christopher Unger <mcu@mac.com>
2026-04-22 21:41:29 -0400

    dev

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -4,6 +4,9 @@ class Certificate < ApplicationRecord
   belongs_to :user
 
   validates :template, inclusion: { in: TEMPLATE_VALUES }, allow_nil: true
+  validate :presented_on_must_be_valid_date
+
+  before_validation :normalize_presented_on
 
   store_accessor(:data,
     :graduate_name,
@@ -15,4 +18,29 @@ class Certificate < ApplicationRecord
     :presented_on,
     :signature_path
   )
+
+  private
+
+  def normalize_presented_on
+    value = presented_on
+    return if value.blank?
+
+    if value.is_a?(Date)
+      self.presented_on = value.to_s
+    elsif value.is_a?(String)
+      self.presented_on = Date.parse(value).to_s
+    elsif value.respond_to?(:to_date)
+      self.presented_on = value.to_date.to_s
+    end
+  rescue ArgumentError, TypeError
+    # Keep the original value so validation can add an error instead of silently clearing it.
+  end
+
+  def presented_on_must_be_valid_date
+    return if presented_on.blank?
+
+    Date.parse(presented_on)
+  rescue ArgumentError, TypeError
+    errors.add(:presented_on, "must be a valid date")
+  end
 end
